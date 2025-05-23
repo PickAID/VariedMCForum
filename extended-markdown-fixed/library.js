29942096c861217897fab4f61e035419e3e98b51
Customer service is currently offline. <82386665+M1hono@users.noreply.github.com>
2025-05-24 07:07:19 +1000

    Try try my fix.

---
+++ extended-markdown-fixed/library.js
@@ -0,0 +1,195 @@
+'use strict';
+
+const slugify = require.main.require('./src/slugify');
+
+const textHeaderRegex = /<p dir="auto">#([a-zA-Z0-9-]*)\((.*)\)<\/p>/g;
+const tooltipRegex = /(<code.*?>.*?<\/code>)|°(.+?)°\((.+?)\)/g;
+
+const codeTabRegex = /(?:<p dir="auto">={3}group<\/p>\n)((?:<pre><code class=".+">[^]*?<\/code><\/pre>\n){2,})(?:<p dir="auto">={3}<\/p>)/g;
+const langCodeRegex = /<code class="(.+)">/;
+
+const colorRegex = /(<code.*>*?[^]<\/code>)|%\((#[\dA-Fa-f]{6}|rgb\(\d{1,3}, ?\d{1,3}, ?\d{1,3}\)|[a-z]+)\)\[(.+?)]/g;
+
+const paragraphAndHeadingRegex = /<(h[1-6]|p dir="auto")>([^]*?)<\/(h[1-6]|p)>/g;
+
+const noteRegex = /<p dir="auto">!!! (info|warning|important) \[([a-zA-Z0-9]*)\]: ((.|<br \/>\n)*)<\/p>/g;
+
+const spoilerRegex = /(?:<p dir="auto">)(?:\|\|)([^]*?)(?:\|\|)(?:<\/p>)/g;
+
+const noteIcons = {
+    info: 'fa-info-circle',
+    warning: 'fa-exclamation-triangle',
+    important: 'fa-exclamation-circle'
+};
+
+const ExtendedMarkdown = {
+    // post
+    async parsePost(data) {
+        if (data && data.postData && data.postData.content) {
+            data.postData.content = applyExtendedMarkdown(data.postData.content);
+            data.postData.content = applyGroupCode(data.postData.content, data.postData.pid);
+            data.postData.content = applySpoiler(data.postData.content, data.postData.pid);
+        }
+        return data;
+    },
+    // user signature
+    async parseSignature(data) {
+        if (data && data.userData && data.userData.signature) {
+            data.userData.signature = applyExtendedMarkdown(data.userData.signature);
+        }
+        return data;
+    },
+    // user description
+    async parseAboutMe(data) {
+        if (data) {
+            data = applyExtendedMarkdown(data);
+        }
+        return data;
+    },
+    // direct preview in editor
+    async parseRaw(data) {
+        if (data) {
+            data = applyExtendedMarkdown(data);
+            data = applyGroupCode(data, "");
+            data = applySpoiler(data, "");
+        }
+        return data;
+    },
+    async registerFormatting(payload) {
+        const formatting = [
+            { name: "color", className: "fa fa-eyedropper", title: "[[extendedmarkdown:composer.formatting.color]]" },
+            { name: "left", className: "fa fa-align-left", title: "[[extendedmarkdown:composer.formatting.left]]" },
+            { name: "center", className: "fa fa-align-center", title: "[[extendedmarkdown:composer.formatting.center]]" },
+            { name: "right", className: "fa fa-align-right", title: "[[extendedmarkdown:composer.formatting.right]]" },
+            { name: "justify", className: "fa fa-align-justify", title: "[[extendedmarkdown:composer.formatting.justify]]" },
+            { name: "textheader", className: "fa fa-header", title: "[[extendedmarkdown:composer.formatting.textheader]]" },
+            { name: "groupedcode", className: "fa fa-file-code-o", title: "[[extendedmarkdown:composer.formatting.groupedcode]]" },
+            { name: "bubbleinfo", className: "fa fa-info-circle", title: "[[extendedmarkdown:composer.formatting.bubbleinfo]]" },
+            { name: "spoiler", className: "fa fa-eye-slash", title: "[[extendedmarkdown:composer.formatting.spoiler]]" },
+            { name: "noteinfo", className: "fa fa-info", title: "[[extendedmarkdown:composer.formatting.noteinfo]]" },
+            { name: "notewarning", className: "fa fa-exclamation-triangle", title: "[[extendedmarkdown:composer.formatting.notewarning]]" },
+            { name: "noteimportant", className: "fa fa-exclamation-circle", title: "[[extendedmarkdown:composer.formatting.noteimportant]]" }
+        ];
+
+        payload.options = payload.options.concat(formatting);
+
+        return payload;
+    },
+    async sanitizerConfig(config) {
+        config.allowedAttributes['a'].push('name');
+        return config;
+    }
+};
+
+function applyExtendedMarkdown(textContent) {
+    if (textContent.match(noteRegex)) {
+        textContent = textContent.replace(noteRegex, function (match, type, title, text) {
+            return `<div class="admonition ${type.toLowerCase()}"><p class="admonition-title"><i class="fa ${noteIcons[type.toLowerCase()]}"></i>${title}</p><p>${text}</p></div>`;
+        });
+    }
+
+    if (textContent.match(textHeaderRegex)) {
+        textContent = textContent.replace(textHeaderRegex, function (match, anchorId, text) {
+            return `<h2 class="text-header"><a class="anchor-offset" name="${anchorId}"></a>${text}</h2>`;
+        });
+    }
+
+    if (textContent.match(tooltipRegex)) {
+        textContent = textContent.replace(tooltipRegex, function (match, code, text, tooltipText) {
+            if (typeof (code) !== "undefined") {
+                return code;
+            } else if ("fa-info" === text) {
+                return `<i class="fa fa-info-circle extended-markdown-tooltip" data-bs-toggle="tooltip" data-bs-placement="top" title="${tooltipText}"></i>`;
+            } else {
+                return `<span class="extended-markdown-tooltip" data-bs-toggle="tooltip" data-bs-placement="top" title="${tooltipText}">${text}</span>`;
+            }
+        });
+    }
+
+    if (textContent.match(colorRegex)) {
+        textContent = textContent.replace(colorRegex, function (match, code, color, text) {
+            if (typeof (code) !== "undefined") {
+                return code;
+            }
+            return `<span style="color: ${color};">${text}</span>`;
+        });
+    }
+
+    if (textContent.match(paragraphAndHeadingRegex)) {
+        textContent = textContent.replace(paragraphAndHeadingRegex, function (match, tag, text, closeTag) {
+            let anchor = tag.charAt(0) == "h" ? generateAnchorFromHeading(text) : "";
+            
+            if (text.startsWith("|=") && text.endsWith("=|")) {
+                const cleanText = text.slice(2, -2);
+                return `<${tag} style="text-align:justify;">${anchor}${cleanText}</${closeTag}>`;
+            } else if (text.startsWith("|-") && text.endsWith("-|")) {
+                const cleanText = text.slice(2, -2);
+                return `<${tag} style="text-align:center;">${anchor}${cleanText}</${closeTag}>`;
+            } else if (text.endsWith("-|")) {
+                const cleanText = text.slice(0, -2);
+                return `<${tag} style="text-align:right;">${anchor}${cleanText}</${closeTag}>`;
+            } else if (text.startsWith("|-")) {
+                const cleanText = text.slice(2);
+                return `<${tag} style="text-align:left;">${anchor}${cleanText}</${closeTag}>`;
+            }
+            return `<${tag}>${anchor}${text}</${closeTag}>`;
+        });
+    }
+
+    return textContent;
+}
+
+function applyGroupCode(textContent, id) {
+    if (textContent.match(codeTabRegex)) {
+        let count = 0;
+        textContent = textContent.replace(codeTabRegex, (match, codes) => {
+            let codeArray = codes.substring(5, codes.length - 6).split(/<\/pre>\n<pre>/g);
+            let lang = [];
+            for (let i in codeArray) {
+                lang[i] = langCodeRegex.exec(codeArray[i])[1];
+                codeArray[i] = "<pre>" + codeArray[i] + "</pre>\n";
+            }
+            let menuTab = "<ul class='nav nav-tabs' role='tablist'>";
+            let contentTab = "<div class='tab-content'>";
+            for (let i = 0; i < lang.length; i++) {
+                menuTab += `<li class="nav-item" role="presentation"><button class="nav-link ${i === 0 ? "active" : ""}" id="${lang[i] + count + id}-tab" data-bs-toggle="tab" data-bs-target="#${lang[i] + count + id}" type="button" role="tab" aria-controls="${lang[i] + count + id}" aria-selected="${i === 0 ? "true" : "false"}">${capitalizeFirstLetter(lang[i])}</button></li>`;
+                contentTab += `<div class="tab-pane fade ${i === 0 ? "show active" : ""}" id="${lang[i] + count + id}" role="tabpanel" aria-labelledby="${lang[i] + count + id}-tab">${codeArray[i]}</div>`;
+            }
+            menuTab += "</ul>";
+            contentTab += "</div>";
+            count++;
+            return menuTab + contentTab;
+        });
+    }
+    return textContent;
+}
+
+function capitalizeFirstLetter(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+function generateAnchorFromHeading(heading) {
+    return `<a class="anchor-offset" name="${slugify(heading)}"></a>`;
+}
+
+function applySpoiler(textContent, id) {
+    if (textContent.match(spoilerRegex)) {
+        let count = 0;
+        textContent = textContent.replace(spoilerRegex, (match, text) => {
+            const spoilerButton = `
+                <button class="btn btn-primary extended-markdown-spoiler" type="button" name="spoiler" data-bs-toggle="collapse" data-bs-target="#spoiler${count + id}" aria-expanded="false" aria-controls="spoiler${count + id}">
+                    Spoiler <i class="fa fa-eye-slash"></i>
+                </button>`;
+
+            const spoilerContent = `
+                <div class="collapse" id="spoiler${count + id}">
+                    <div class="card card-body spoiler"><p dir="auto">${text}</p></div>
+                </div>`;
+            count++;
+            return `<p>${spoilerButton}${spoilerContent}</p>`;
+        });
+    }
+    return textContent;
+}
+
+module.exports = ExtendedMarkdown;
---
+++ extended-markdown-fixed/public/js/client.js
@@ -0,0 +1,178 @@
+"use strict";
+
+/* global $ */
+
+$(document).ready(function () {
+    var ExtendedMarkdown = {};
+
+    pageReady();
+
+    $(window).on('action:ajaxify.end', function (ev, data) {
+        pageReady();
+    });
+
+    $(window).on('action:composer.enhanced', function (evt, data) {
+        ExtendedMarkdown.prepareFormattingTools();
+    });
+
+    ExtendedMarkdown.prepareFormattingTools = async function () {
+        const [formatting, controls, translator] = await app.require(['composer/formatting', 'composer/controls', 'translator']);
+        if (formatting && controls) {
+            translator.getTranslations(window.config.userLang || window.config.defaultLang, 'extendedmarkdown', function (strings) {
+                var composerTextarea;
+                var colorPickerButton = document.querySelector('.btn[data-format="color"]');
+                var hiddenPicker = document.createElement("input");
+                hiddenPicker.style.visibility = 'hidden';
+                hiddenPicker.style.width = 0;
+                hiddenPicker.style.padding = 0;
+                hiddenPicker.style.margin = 0;
+                hiddenPicker.style.height = 0;
+                hiddenPicker.style.border = 0;
+                hiddenPicker.type = 'color';
+                hiddenPicker.id = 'nodebb-plugin-extended-markdown-colorpicker';
+                colorPickerButton.parentNode.insertBefore(hiddenPicker, colorPickerButton.nextSibling);
+                hiddenPicker.addEventListener('input', function() {
+                    var selectionStart = composerTextarea.selectionStart;
+                    var selectionEnd = composerTextarea.selectionEnd;
+                    composerTextarea.value = composerTextarea.value.slice(0, selectionStart) + this.value + composerTextarea.value.slice(selectionEnd);
+                    $(composerTextarea).trigger('propertychange');
+                    composerTextarea.selectionStart = selectionStart;
+                    composerTextarea.selectionEnd = selectionEnd;
+                });
+
+                formatting.addButtonDispatch('color', function (textarea, selectionStart, selectionEnd) {
+                    if (selectionStart === selectionEnd) {
+                        controls.insertIntoTextarea(textarea, '%(#000000)[' + strings.color_text + ']');
+                        controls.updateTextareaSelection(textarea, selectionStart + 2, selectionStart + 9);
+                    } else {
+                        var wrapDelta = controls.wrapSelectionInTextareaWith(textarea, '%(#000000)[', ']');
+                        controls.updateTextareaSelection(textarea, selectionStart + 2, selectionStart + 9);
+                    }
+                    composerTextarea = textarea;
+                    hiddenPicker.click();
+                });
+
+                formatting.addButtonDispatch('left', function (textarea, selectionStart, selectionEnd) {
+                    if (selectionStart === selectionEnd) {
+                        controls.insertIntoTextarea(textarea, '|-' + strings.align_left);
+                        controls.updateTextareaSelection(textarea, selectionStart + 2, selectionStart + 2 + strings.align_left.length);
+                    } else {
+                        controls.wrapSelectionInTextareaWith(textarea, '|-', '');
+                        controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 2);
+                    }
+                });
+
+                formatting.addButtonDispatch('center', function (textarea, selectionStart, selectionEnd) {
+                    if (selectionStart === selectionEnd) {
+                        controls.insertIntoTextarea(textarea, '|-' + strings.align_center + '-|');
+                        controls.updateTextareaSelection(textarea, selectionStart + 2, selectionStart + 2 + strings.align_center.length);
+                    } else {
+                        var wrapDelta = controls.wrapSelectionInTextareaWith(textarea, '|-', '-|');
+                        controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 2);
+                    }
+                });
+
+                formatting.addButtonDispatch('right', function (textarea, selectionStart, selectionEnd) {
+                    if (selectionStart === selectionEnd) {
+                        controls.insertIntoTextarea(textarea, strings.align_right + '-|');
+                        controls.updateTextareaSelection(textarea, selectionStart, selectionStart + strings.align_right.length);
+                    } else {
+                        controls.wrapSelectionInTextareaWith(textarea, '', '-|');
+                        controls.updateTextareaSelection(textarea, selectionStart, selectionEnd);
+                    }
+                });
+
+                formatting.addButtonDispatch('justify', function (textarea, selectionStart, selectionEnd) {
+                    if (selectionStart === selectionEnd) {
+                        controls.insertIntoTextarea(textarea, '|=' + strings.align_justify + '=|');
+                        controls.updateTextareaSelection(textarea, selectionStart + 2, selectionStart + 2 + strings.align_justify.length);
+                    } else {
+                        controls.wrapSelectionInTextareaWith(textarea, '|=', '=|');
+                        controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 2);
+                    }
+                });
+
+                formatting.addButtonDispatch('textheader', function (textarea, selectionStart, selectionEnd) {
+                    if (selectionStart === selectionEnd) {
+                        controls.insertIntoTextarea(textarea, '#' + strings.textheader_anchor + '(' + strings.textheader_title + ')');
+                        controls.updateTextareaSelection(textarea, selectionStart + 1, selectionStart + strings.textheader_anchor.length + 1);
+                    } else {
+                        var wrapDelta = controls.wrapSelectionInTextareaWith(textarea, '#' + strings.textheader_anchor + '(', ')');
+                        controls.updateTextareaSelection(textarea, selectionStart + 1, selectionStart + strings.textheader_anchor.length + 1);
+                    }
+                });
+
+                formatting.addButtonDispatch('groupedcode', function (textarea, selectionStart, selectionEnd) {
+                    if (selectionStart === selectionEnd) {
+                        controls.insertIntoTextarea(textarea, '===group\n```' + strings.groupedcode_firstlang + "\n```\n```" + strings.groupedcode_secondlang + '\n```\n===');
+                    }
+                });
+
+                formatting.addButtonDispatch('bubbleinfo', function (textarea, selectionStart, selectionEnd) {
+                    if (selectionStart === selectionEnd) {
+                        controls.insertIntoTextarea(textarea, '°fa-info°(' + strings.bubbleinfo_text + ')');
+                        controls.updateTextareaSelection(textarea, selectionStart + 1, selectionStart + 8);
+                    } else {
+                        var wrapDelta = controls.wrapSelectionInTextareaWith(textarea, '°', '°(' + strings.bubbleinfo_text + ')');
+                        controls.updateTextareaSelection(textarea, selectionEnd + 3 - wrapDelta[1], selectionEnd + strings.bubbleinfo_text.length + 3 - wrapDelta[1]);
+                    }
+                });
+                
+                formatting.addButtonDispatch('spoiler', function (textarea, selectionStart, selectionEnd) {
+                    if (selectionStart === selectionEnd) {
+                        controls.insertIntoTextarea(textarea, "||" + strings.spoiler + "||");
+                        controls.updateTextareaSelection(textarea, selectionStart + 2, selectionStart + 2 + strings.spoiler.length);
+                    } else {
+                        controls.wrapSelectionInTextareaWith(textarea, "||", "||");
+                        controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 2);
+                    }
+                });
+
+                formatting.addButtonDispatch('noteinfo', function (textarea, selectionStart, selectionEnd) {
+                    if (selectionStart === selectionEnd) {
+                        controls.insertIntoTextarea(textarea, '!!! info [' + strings.note_title + ']: ' + strings.note_content);
+                        controls.updateTextareaSelection(textarea, selectionStart + 11, selectionStart + 11 + strings.note_title.length);
+                    } else {
+                        controls.wrapSelectionInTextareaWith(textarea, '!!! info [' + strings.note_title + ']: ', '');
+                        controls.updateTextareaSelection(textarea, selectionStart + 11, selectionStart + 11 + strings.note_title.length);
+                    }
+                });
+
+                formatting.addButtonDispatch('notewarning', function (textarea, selectionStart, selectionEnd) {
+                    if (selectionStart === selectionEnd) {
+                        controls.insertIntoTextarea(textarea, '!!! warning [' + strings.note_title + ']: ' + strings.note_content);
+                        controls.updateTextareaSelection(textarea, selectionStart + 14, selectionStart + 14 + strings.note_title.length);
+                    } else {
+                        controls.wrapSelectionInTextareaWith(textarea, '!!! warning [' + strings.note_title + ']: ', '');
+                        controls.updateTextareaSelection(textarea, selectionStart + 14, selectionStart + 14 + strings.note_title.length);
+                    }
+                });
+
+                formatting.addButtonDispatch('noteimportant', function (textarea, selectionStart, selectionEnd) {
+                    if (selectionStart === selectionEnd) {
+                        controls.insertIntoTextarea(textarea, '!!! important [' + strings.note_title + ']: ' + strings.note_content);
+                        controls.updateTextareaSelection(textarea, selectionStart + 16, selectionStart + 16 + strings.note_title.length);
+                    } else {
+                        controls.wrapSelectionInTextareaWith(textarea, '!!! important [' + strings.note_title + ']: ', '');
+                        controls.updateTextareaSelection(textarea, selectionStart + 16, selectionStart + 16 + strings.note_title.length);
+                    }
+                });
+            });
+        }
+    };
+
+    async function pageReady() {
+        require(['bootstrap'], function (bootstrap) {
+            document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(function (element) {
+                new bootstrap.Tooltip(element);
+            });
+        });
+
+        document.querySelectorAll('button.extended-markdown-spoiler').forEach(function (element) {
+            element.onclick = function() {
+                const isExpanded = element.getAttribute("aria-expanded") === "true";
+                element.children[0].className = isExpanded ? "fa fa-eye-slash" : "fa fa-eye";
+            };
+        });
+    }
+});
